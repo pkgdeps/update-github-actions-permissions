1a10ef4f3ac865af01178a9a8723ca55821f43ce
azu <azuciao@gmail.com>
2022-08-04 21:32:46 +0900

    fix: update validator-ts

--- src/types.validator.ts
+++ src/types.validator.ts
@@ -4,80 +4,80 @@
 import Ajv from "ajv";
 import * as apiTypes from "./types";
 
-const SCHEMA = {
+export const SCHEMA = {
     $schema: "http://json-schema.org/draft-07/schema#",
     definitions: {
-        GitHubAction: {
+        GhPermissionValue: {
+            type: "string",
+            enum: ["none", "read", "write"]
+        },
+        GhPermissionTypes: {
+            type: "object",
+            properties: {
+                actions: {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                checks: {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                contents: {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                "pull-requests": {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                deployments: {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                issues: {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                packages: {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                "repository-projects": {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                "security-events": {
+                    $ref: "#/definitions/GhPermissionValue"
+                },
+                statuses: {
+                    $ref: "#/definitions/GhPermissionValue"
+                }
+            },
+            additionalProperties: false
+        },
+        GhPermissions: {
+            anyOf: [
+                {
+                    type: "string",
+                    const: "read-all"
+                },
+                {
+                    type: "string",
+                    const: "write-all"
+                },
+                {
+                    $ref: "#/definitions/GhPermissionTypes"
+                }
+            ]
+        },
+        GhPermissionsDefinition: {
             type: "object",
             properties: {
                 permissions: {
-                    anyOf: [
-                        {
-                            type: "string",
-                            const: "read-all"
-                        },
-                        {
-                            type: "string",
-                            const: "write-all"
-                        },
-                        {
-                            type: "object",
-                            properties: {
-                                actions: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                checks: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                contents: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                "pull-requests": {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                deployments: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                issues: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                packages: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                "repository-projects": {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                "security-events": {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                },
-                                statuses: {
-                                    type: "string",
-                                    enum: ["none", "read", "write"]
-                                }
-                            },
-                            additionalProperties: false
-                        }
-                    ]
+                    $ref: "#/definitions/GhPermissions"
                 }
             },
             required: ["permissions"],
             additionalProperties: false
         },
-        GitHubActions: {
+        GhPermissionsDefinitions: {
             type: "object",
             additionalProperties: {
                 anyOf: [
                     {
-                        $ref: "#/definitions/GitHubAction"
+                        $ref: "#/definitions/GhPermissionsDefinition"
                     },
                     {
                         type: "null"
@@ -87,34 +87,120 @@ const SCHEMA = {
         }
     }
 };
-const ajv = new Ajv().addSchema(SCHEMA, "SCHEMA");
-
-export function validateGitHubAction(payload: unknown): apiTypes.GhPermissionsDefinition {
-    if (!isGitHubAction(payload)) {
-        const error = new Error("invalid payload: GitHubAction");
+const ajv = new Ajv({ removeAdditional: true }).addSchema(SCHEMA, "SCHEMA");
+export function validateGhPermissionValue(payload: unknown): apiTypes.GhPermissionValue {
+    /** Schema is defined in {@link SCHEMA.definitions.GhPermissionValue } **/
+    const validator = ajv.getSchema("SCHEMA#/definitions/GhPermissionValue");
+    const valid = validator(payload);
+    if (!valid) {
+        const error = new Error(
+            "Invalid GhPermissionValue: " + ajv.errorsText(validator.errors, { dataVar: "GhPermissionValue" })
+        );
         error.name = "ValidationError";
         throw error;
     }
     return payload;
 }
 
-export function isGitHubAction(payload: unknown): payload is apiTypes.GhPermissionsDefinition {
-    /** Schema is defined in {@link SCHEMA.definitions.GitHubAction } **/
-    const ajvValidate = ajv.compile({ $ref: "SCHEMA#/definitions/GitHubAction" });
-    return ajvValidate(payload);
+export function isGhPermissionValue(payload: unknown): payload is apiTypes.GhPermissionValue {
+    try {
+        validateGhPermissionValue(payload);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
-export function validateGitHubActions(payload: unknown): apiTypes.GhPermissionsDefinitions {
-    if (!isGitHubActions(payload)) {
-        const error = new Error("invalid payload: GitHubActions");
+export function validateGhPermissionTypes(payload: unknown): apiTypes.GhPermissionTypes {
+    /** Schema is defined in {@link SCHEMA.definitions.GhPermissionTypes } **/
+    const validator = ajv.getSchema("SCHEMA#/definitions/GhPermissionTypes");
+    const valid = validator(payload);
+    if (!valid) {
+        const error = new Error(
+            "Invalid GhPermissionTypes: " + ajv.errorsText(validator.errors, { dataVar: "GhPermissionTypes" })
+        );
         error.name = "ValidationError";
         throw error;
     }
     return payload;
 }
 
-export function isGitHubActions(payload: unknown): payload is apiTypes.GhPermissionsDefinitions {
-    /** Schema is defined in {@link SCHEMA.definitions.GitHubActions } **/
-    const ajvValidate = ajv.compile({ $ref: "SCHEMA#/definitions/GitHubActions" });
-    return ajvValidate(payload);
+export function isGhPermissionTypes(payload: unknown): payload is apiTypes.GhPermissionTypes {
+    try {
+        validateGhPermissionTypes(payload);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+export function validateGhPermissions(payload: unknown): apiTypes.GhPermissions {
+    /** Schema is defined in {@link SCHEMA.definitions.GhPermissions } **/
+    const validator = ajv.getSchema("SCHEMA#/definitions/GhPermissions");
+    const valid = validator(payload);
+    if (!valid) {
+        const error = new Error(
+            "Invalid GhPermissions: " + ajv.errorsText(validator.errors, { dataVar: "GhPermissions" })
+        );
+        error.name = "ValidationError";
+        throw error;
+    }
+    return payload;
+}
+
+export function isGhPermissions(payload: unknown): payload is apiTypes.GhPermissions {
+    try {
+        validateGhPermissions(payload);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+export function validateGhPermissionsDefinition(payload: unknown): apiTypes.GhPermissionsDefinition {
+    /** Schema is defined in {@link SCHEMA.definitions.GhPermissionsDefinition } **/
+    const validator = ajv.getSchema("SCHEMA#/definitions/GhPermissionsDefinition");
+    const valid = validator(payload);
+    if (!valid) {
+        const error = new Error(
+            "Invalid GhPermissionsDefinition: " +
+                ajv.errorsText(validator.errors, { dataVar: "GhPermissionsDefinition" })
+        );
+        error.name = "ValidationError";
+        throw error;
+    }
+    return payload;
+}
+
+export function isGhPermissionsDefinition(payload: unknown): payload is apiTypes.GhPermissionsDefinition {
+    try {
+        validateGhPermissionsDefinition(payload);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+export function validateGhPermissionsDefinitions(payload: unknown): apiTypes.GhPermissionsDefinitions {
+    /** Schema is defined in {@link SCHEMA.definitions.GhPermissionsDefinitions } **/
+    const validator = ajv.getSchema("SCHEMA#/definitions/GhPermissionsDefinitions");
+    const valid = validator(payload);
+    if (!valid) {
+        const error = new Error(
+            "Invalid GhPermissionsDefinitions: " +
+                ajv.errorsText(validator.errors, { dataVar: "GhPermissionsDefinitions" })
+        );
+        error.name = "ValidationError";
+        throw error;
+    }
+    return payload;
+}
+
+export function isGhPermissionsDefinitions(payload: unknown): payload is apiTypes.GhPermissionsDefinitions {
+    try {
+        validateGhPermissionsDefinitions(payload);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
